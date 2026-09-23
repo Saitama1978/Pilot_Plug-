1b3752eb4b7786b28f83a130862908293632fccd
Choy_Vlog <98365296+Saitama1978@users.noreply.github.com>
2026-09-23 07:07:11 -0300

    Update main function to initialize tile caching
    Initialize tile caching settings before running the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FMTCObjectProvider.settings.init();
   runApp(const PilotPlugApp());
 }
 
@@ -247,6 +250,7 @@ class _PilotageScreenState extends State<PilotageScreen> {
                       TileLayer(
                         urlTemplate: 'https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
                         userAgentPackageName: 'com.example.pilot_plug',
+                        tileProvider: FMTC.instance('mapStore').getTileProvider(),
                       ),
                       MarkerLayer(
                         markers: [
